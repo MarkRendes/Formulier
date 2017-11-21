b38da833a4a9ccd0ae9626933df7e3cecf7669bc
MarkRendes <31918883+MarkRendes@users.noreply.github.com>
2017-11-21 13:35:38 +0100

    Iets

--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Threading;
 
 namespace WpfApp1
 {
@@ -23,6 +24,35 @@ namespace WpfApp1
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            
+            lblStart.Content = "Hello! What's up?";
+            
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            lblClose.Content = "Why do you want to close me? :(";
+        }
+
+        private void Like_Checked(object sender, RoutedEventArgs e)
+        {
+                lblCheck.Content = "I know you don't...";    
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            lblOk.Content = "Why are you asking a program about this?";
+
+
+        }
+
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+                pgpMyprogressbar.Value += 20;  
         }
     }
 }
